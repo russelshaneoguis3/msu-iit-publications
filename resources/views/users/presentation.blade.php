b2224a47e0813276b4a727deb0dcf453f54f9b75
russelshaneoguis3 <russelshaneoguis3@gmail.com>
2024-11-22 15:45:16 +0800

    Final Touches

--- resources/views/users/presentation.blade.php
+++ resources/views/users/presentation.blade.php
@@ -165,7 +165,34 @@
 
                     <div class="form-group">
                         <label for="date_duration">Research Duration <span> (in months, e.g. 10 months)</span></label>
-                        <input type="text" class="form-control" id="date_duration" name="date_duration">
+                        <select class="form-select" id="date_duration" name="date_duration">
+                            <option value="">-- Select Here --</option>
+                            <option value="1 month">1 month</option>
+                            <option value="2 months">2 months</option>
+                            <option value="3 months">3 months</option>
+                            <option value="4 months">4 months</option>
+                            <option value="5 months">5 months</option>
+                            <option value="6 months">6 months</option>
+                            <option value="7 months">7 months</option>
+                            <option value="8 months">8 months</option>
+                            <option value="9 months">9 months</option>
+                            <option value="10 months">10 months</option>
+                            <option value="11 months">11 months</option>
+                            <option value="12 months">12 months</option>
+                            <option value="13 months">13 months</option>
+                            <option value="14 months">14 months</option>
+                            <option value="15 months">15 months</option>
+                            <option value="16 months">16 months</option>
+                            <option value="17 months">17 months</option>
+                            <option value="18 months">18 months</option>
+                            <option value="19 months">19 months</option>
+                            <option value="20 months">20 months</option>
+                            <option value="21 months">21 months</option>
+                            <option value="22 months">22 months</option>
+                            <option value="23 months">23 months</option>
+                            <option value="24 months">24 months</option>
+                            <option value="24+ months">24+ months</option>
+                        </select>
                     </div><br>
 
                     <div class="form-group">
@@ -430,7 +457,34 @@ Add Presentation
 
                     <div class="form-group">
                         <label for="date_duration">Research Duration <span> (in months, e.g. 10 months)</span></label>
-                        <input type="text" class="form-control" id="date_duration" name="date_duration" value="{{ $userpresent->date_duration }}">
+                        <select class="form-select" id="date_duration" name="date_duration">
+                            <option value="">-- Select Here --</option>
+                            <option value="1 month" {{ $userpresent->date_duration == '1 month' ? 'selected' : '' }}>1 month</option>
+                            <option value="2 months" {{ $userpresent->date_duration == '2 months' ? 'selected' : '' }}>2 months</option>
+                            <option value="3 months" {{ $userpresent->date_duration == '3 months' ? 'selected' : '' }}>3 months</option>
+                            <option value="4 months" {{ $userpresent->date_duration == '4 months' ? 'selected' : '' }}>4 months</option>
+                            <option value="5 months" {{ $userpresent->date_duration == '5 months' ? 'selected' : '' }}>5 months</option>
+                            <option value="6 months" {{ $userpresent->date_duration == '6 months' ? 'selected' : '' }}>6 months</option>
+                            <option value="7 months" {{ $userpresent->date_duration == '7 months' ? 'selected' : '' }}>7 months</option>
+                            <option value="8 months" {{ $userpresent->date_duration == '8 months' ? 'selected' : '' }}>8 months</option>
+                            <option value="9 months" {{ $userpresent->date_duration == '9 months' ? 'selected' : '' }}>9 months</option>
+                            <option value="10 months" {{ $userpresent->date_duration == '10 months' ? 'selected' : '' }}>10 months</option>
+                            <option value="11 months" {{ $userpresent->date_duration == '11 months' ? 'selected' : '' }}>11 months</option>
+                            <option value="12 months" {{ $userpresent->date_duration == '12 months' ? 'selected' : '' }}>12 months</option>
+                            <option value="13 months" {{ $userpresent->date_duration == '13 months' ? 'selected' : '' }}>13 months</option>
+                            <option value="14 months" {{ $userpresent->date_duration == '14 months' ? 'selected' : '' }}>14 months</option>
+                            <option value="15 months" {{ $userpresent->date_duration == '15 months' ? 'selected' : '' }}>15 months</option>
+                            <option value="16 months" {{ $userpresent->date_duration == '16 months' ? 'selected' : '' }}>16 months</option>
+                            <option value="17 months" {{ $userpresent->date_duration == '17 months' ? 'selected' : '' }}>17 months</option>
+                            <option value="18 months" {{ $userpresent->date_duration == '18 months' ? 'selected' : '' }}>18 months</option>
+                            <option value="19 months" {{ $userpresent->date_duration == '19 months' ? 'selected' : '' }}>19 months</option>
+                            <option value="20 months" {{ $userpresent->date_duration == '20 months' ? 'selected' : '' }}>20 months</option>
+                            <option value="21 months" {{ $userpresent->date_duration == '21 months' ? 'selected' : '' }}>21 months</option>
+                            <option value="22 months" {{ $userpresent->date_duration == '22 months' ? 'selected' : '' }}>22 months</option>
+                            <option value="23 months" {{ $userpresent->date_duration == '23 months' ? 'selected' : '' }}>23 months</option>
+                            <option value="24 months" {{ $userpresent->date_duration == '24 months' ? 'selected' : '' }}>24 months</option>
+                            <option value="24+ months" {{ $userpresent->date_duration == '24+ months' ? 'selected' : '' }}>24+ months</option>
+                        </select>
                     </div><br>
 
                     <div class="form-group">
